877bfa2d7884d461533b6ada34a25e833059b912
Andrew Shell <andrew@andrewshell.org>
2016-10-20 08:00:53 -0500

    Atlas changed setConnectionLocator, updating to reflect

--- src/AtlasContainer.php
+++ src/AtlasContainer.php
@@ -8,9 +8,34 @@ class AtlasContainer extends BaseAtlasContainer
 {
     protected function setConnectionLocator(array $args)
     {
-        $this->connectionLocator = new ConnectionLocator();
-        $this->connectionLocator->setDefault(function () use ($args) {
-            return new ExtendedPdo(...$args);
-        });
+        switch (true) {
+
+            case $args[0] instanceof ExtendedPdo:
+                $extendedPdo = $args[0];
+                $default = function () use ($extendedPdo) {
+                    return $extendedPdo;
+                };
+                $driver = $extendedPdo->getAttribute(PDO::ATTR_DRIVER_NAME);
+                break;
+
+            case $args[0] instanceof PDO:
+                $extendedPdo = $args[0];
+                $default = function () use ($pdo) {
+                    return new ExtendedPdo($pdo);
+                };
+                $driver = $pdo->getAttribute(ExtendedPdo::ATTR_DRIVER_NAME);
+                break;
+
+            default:
+                $default = function () use ($args) {
+                    return new ExtendedPdo(...$args);
+                };
+                $parts = explode(':', $args[0]);
+                $driver = array_shift($parts);
+                break;
+        }
+
+        $this->connectionLocator = new ConnectionLocator($default);
+        return $driver;
     }
 }
